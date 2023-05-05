caf8580a6a78d213dc4085e34313f56b018bc3b6
Luca-Sordetti <91209271+Luca-Sordetti@users.noreply.github.com>
2023-05-05 10:35:44 +0200

    feat(cache): added cache

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,20 +6,43 @@ use App\Entity\Task;
 use App\Form\TaskType;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
+    /**
+     * Display the list of tasks
+     *
+     * @param TaskRepository $taskRepository
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/tasks', name: 'task_list')]
-    public function listAction(TaskRepository $taskRepository)
+    public function listAction(TaskRepository $taskRepository, TagAwareCacheInterface $cache)
     {
-        return $this->render('task/list.html.twig', ['tasks' => $taskRepository->findAll()]);
+        $tasks = $cache->get('tasks', function (ItemInterface $item) use ($taskRepository) {
+            $item->tag("tasks");
+            return $taskRepository->findAll();
+        });
+
+
+        return $this->render('task/list.html.twig', ['tasks' => $tasks]);
     }
 
+    /**
+     * Create a new task
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/tasks/create', name: 'task_create')]
-    public function createAction(Request $request, EntityManagerInterface $em)
+    public function createAction(Request $request, EntityManagerInterface $em, TagAwareCacheInterface $cache)
     {
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
@@ -33,14 +56,25 @@ class TaskController extends AbstractController
 
             $this->addFlash('success', 'La tâche a été bien été ajoutée.');
 
+            $cache->invalidateTags(["tasks"]);
+
             return $this->redirectToRoute('task_list');
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 
+    /**
+     * Edit a task
+     * 
+     * @param Task $task
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/tasks/{id}/edit', name: 'task_edit')]
-    public function editAction(Task $task, Request $request, EntityManagerInterface $em)
+    public function editAction(Task $task, Request $request, EntityManagerInterface $em, TagAwareCacheInterface $cache)
     {
         $form = $this->createForm(TaskType::class, $task);
 
@@ -51,6 +85,8 @@ class TaskController extends AbstractController
 
             $this->addFlash('success', 'La tâche a bien été modifiée.');
 
+            $cache->invalidateTags(["tasks"]);
+
             return $this->redirectToRoute('task_list');
         }
 
@@ -60,19 +96,37 @@ class TaskController extends AbstractController
         ]);
     }
 
+    /**
+     * Toggle a task
+     * 
+     * @param Task $task
+     * @param EntityManagerInterface $em
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/tasks/{id}/toggle', name: 'task_toggle')]
-    public function toggleTaskAction(Task $task, EntityManagerInterface $em)
+    public function toggleTaskAction(Task $task, EntityManagerInterface $em, TagAwareCacheInterface $cache)
     {
         $task->toggle(!$task->isDone());
         $em->flush();
 
         $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
 
+        $cache->invalidateTags(["tasks"]);
+
         return $this->redirectToRoute('task_list');
     }
 
+    /**
+     * Delete a task
+     * 
+     * @param Task $task
+     * @param EntityManagerInterface $em
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/tasks/{id}/delete', name: 'task_delete')]
-    public function deleteTaskAction(Task $task, EntityManagerInterface $em)
+    public function deleteTaskAction(Task $task, EntityManagerInterface $em, TagAwareCacheInterface $cache)
     {
         if( $task->getUser() !== $this->getUser() && !$this->isGranted('ROLE_ADMIN') ) {
             $this->addFlash('error', 'Vous n\'avez pas le droit de supprimer cette tâche.');
@@ -83,6 +137,8 @@ class TaskController extends AbstractController
         $em->flush();
 
         $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        $cache->invalidateTags(["tasks"]);
 
         return $this->redirectToRoute('task_list');
     }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,21 +6,44 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    /**
+     * Display the list of users
+     *
+     * @param UserRepository $userRepository
+     * @param TagAwareCacheInterface $item
+     * @return void
+     */
     #[Route('/users', name: 'user_list')]
-    public function listAction(UserRepository $userRepository)
+    public function listAction(UserRepository $userRepository, TagAwareCacheInterface $item)
     {
-        return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
+        $users = $item->get('users', function (ItemInterface $item) use ($userRepository) {
+            $item->tag("users");
+            return $userRepository->findAll();
+        });
+
+        return $this->render('user/list.html.twig', ['users' => $users]);
     }
 
+    /**
+     * Create a new user
+     *
+     * @param Request $request
+     * @param UserPasswordHasherInterface $passwordHasher
+     * @param EntityManagerInterface $em
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/users/create', name: 'user_create')]
-    public function createAction(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em)
+    public function createAction(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, TagAwareCacheInterface $cache)
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -36,14 +59,26 @@ class UserController extends AbstractController
 
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
 
+            $cache->invalidateTags(["users"]);
+
             return $this->redirectToRoute('user_list');
         }
 
         return $this->render('user/create.html.twig', ['form' => $form->createView()]);
     }
 
+    /**
+     * Edit an existing user
+     * 
+     * @param User $user
+     * @param Request $request
+     * @param UserPasswordHasherInterface $passwordHasher
+     * @param EntityManagerInterface $em
+     * @param TagAwareCacheInterface $cache
+     * @return void
+     */
     #[Route('/users/{id}/edit', name: 'user_edit')]
-    public function editAction(User $user, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em)
+    public function editAction(User $user, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, TagAwareCacheInterface $cache)
     {
         $form = $this->createForm(UserType::class, $user);
 
@@ -56,6 +91,8 @@ class UserController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            $cache->invalidateTags(["users"]);
 
             return $this->redirectToRoute('user_list');
         }
